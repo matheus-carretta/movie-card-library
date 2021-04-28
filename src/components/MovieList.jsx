f6beaea7866e1dcb98a7ba2c11e0d09a84f1f897
Matheus Carretta <matheuscarretta@gmail.com>
2021-04-28 18:36:25 -0300

    Adiciona prop-types e faz requisito 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <p>ok</p>
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } />) }
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storylane: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
