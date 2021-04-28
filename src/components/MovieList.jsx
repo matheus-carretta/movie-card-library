ec70fd4736f381b5c270d0df536758a110a6ec43
Matheus Carretta <matheuscarretta@gmail.com>
2021-04-28 17:36:08 -0300

    Requisito 3 concluido

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p>ok</p>
+    );
+  }
+}
+
+export default MovieList;
