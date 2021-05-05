f3fdc75ec9bb919b287fe5f7fef6806a9492d8ad
Matheus Carretta <matheuscarretta@gmail.com>
2021-05-05 19:00:27 -0300

    Updates

---
+++ precache-manifest.43820503f60f0d863826d5bcdfe99c79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62158a5b607276cf0be593dc7a5f480e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b9f35887ac580e5a1c",
+    "url": "/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "d9b9f35887ac580e5a1c",
+    "url": "/static/js/main.42ef9346.chunk.js"
+  },
+  {
+    "revision": "ead9e25ff29b47e65f9c",
+    "url": "/static/js/runtime-main.d2351037.js"
+  }
+]);
